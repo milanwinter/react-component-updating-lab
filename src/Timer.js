445b5503fd9145e51877d36e44284de5df3c88e6
milanwinter <milanwinter8@gmail.com>
2021-02-05 11:55:57 -0800

    commit tree ee81a1a632124454ff68e3cd733b795a0d03b5e0

--- src/Timer.js
+++ src/Timer.js
@@ -10,7 +10,10 @@ class Timer extends Component {
     };
   }
 
-  //Your code here
+  componentDidUpdate() {
+    this.timer.current.style.color =
+  "#" + Math.floor(Math.random() * 16777215).toString(16);
+  }
 
   componentDidMount() {
     this.interval = setInterval(
@@ -23,6 +26,12 @@ class Timer extends Component {
     clearInterval(this.interval);
   }
 
+  shouldComponentUpdate(nextProps,nextState) {
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
+  }
   render() {
     const { time, color, logText } = this.state;
     return (
